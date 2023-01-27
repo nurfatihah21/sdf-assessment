09eee2375e6852513535fa7f1cb4b93240545e00
nurfatihah21 <nurfatihah.r@gmail.com>
2023-01-27 09:25:31 +0800

    read file

--- task01/src/task01/Main.java
+++ task01/src/task01/Main.java
@@ -1,9 +1,32 @@
 package task01;
 
-public class Main{
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
-public static void main(String[] args) {
-    
-}
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+         * to run java file, enter argument e.g. file name at the end
+         */
+
+        Console cons = System.console();
+        BufferedReader reader = null;
+
+        // User will input the file name
+
+        String fileName = args[0];
+        Path filePath = Paths.get("./files/" + fileName);
+        File file = filePath.toFile();
+
+        if (file.exists()) {
+            System.out.printf("Opening file >> %s\n", fileName);
+        } else {
+            System.out.printf("File doesn't exist >> %s\n", fileName);
+        }
+
+    }
 
 }
