022a0d3e210792c2c96f50e440fee94c0c2d53e7
nurfatihah21 <nurfatihah.r@gmail.com>
2023-01-27 09:53:17 +0800

    read file & processed contents

--- task01/src/task01/Main.java
+++ task01/src/task01/Main.java
@@ -6,26 +6,40 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         /*
          * to run java file, enter argument e.g. file name at the end
          */
 
         Console cons = System.console();
-        BufferedReader reader = null;
 
         // User will input the file name
+        // If file exists, print out 'opening'
+        // If doesn't exist, print out 'doesnt exist'
 
         String fileName = args[0];
         Path filePath = Paths.get("./files/" + fileName);
-        File file = filePath.toFile();
+        File fileToRead = filePath.toFile();
 
-        if (file.exists()) {
+        if (fileToRead.exists()) {
             System.out.printf("Opening file >> %s\n", fileName);
         } else {
             System.out.printf("File doesn't exist >> %s\n", fileName);
         }
+
+        // Reading the file's content
+
+        FileReader fr = new FileReader(fileToRead);
+        BufferedReader br = new BufferedReader(fr);
+        String line = "";
+        String lineProcessed = "";
+
+        while ((line = br.readLine()) != null) {
+            lineProcessed = line.toLowerCase().replaceAll("[.,:!-(){}'/\";]", "");
+            System.out.println(lineProcessed);
+        }
+        br.close();
 
     }
 
